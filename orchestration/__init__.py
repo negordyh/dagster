9e2a190b58fd1352f2db2814ab544fda03bfe514
negordyh <vanduyen.mai@gmail.com>
2024-07-12 10:01:57 +0700

    WIP

--- orchestration/__init__.py
+++ orchestration/__init__.py
@@ -1,17 +1,17 @@
+# __init__.py
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import example_asset
-# from .jobs import example_job
-# from .schedules import example_schedule
-from .resources import example_resource
-
+from orchestration.assets import example_asset
+from orchestration.jobs.example_job import example_job
+from orchestration.schedules.example_schedule import example_schedule
+from orchestration.resources.example_resource import example_resource
 
 all_assets = load_assets_from_modules([example_asset])
 
 defs = Definitions(
     assets=[*all_assets],
-    # jobs=[example_job],
-    # schedules=[example_schedule],
+    jobs=[example_job],
+    schedules=[example_schedule],
     resources={
         "example_resource": example_resource,
     }
